e2d71318934a385b09d18aa26f4b69fbdb83f430
Ninerian <hallo@daniel-der-grosse.de>
2016-08-08 23:19:40 +0200

    Delete packages

--- _elpa/xref-js2-1.3/xref-js2-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; xref-js2-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("xref-js2.el") (22436 60246 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; xref-js2-autoloads.el ends here
